42009a61a43bcc2b98a3b8449da7a899118db24f
saturn7601 <57055019+saturn7601@users.noreply.github.com>
2019-10-27 05:13:18 -0500

    Add files via upload

---
+++ api.py
@@ -0,0 +1,26 @@
+import googlemaps
+from geocoding import geoloc_list_lat
+from geocoding import geoloc_list_lng
+API_KEY = 'AIzaSyBYO9oQNYHnZEeFZxHL2__garWzcC3Poks'
+google_maps = googlemaps.Client(key=API_KEY)
+
+result = []
+
+def placeapi():
+    for i in range(len(geoloc_list_lng)):
+        places= []
+        combined = ''
+        cordnate = str(geoloc_list_lat[i]) + ',' + str(geoloc_list_lng[i])
+        try:
+            places= gmaps.places_nearby(location=cordnate, radius=300, open_now=False, type='restaurant')
+        except KeyError:
+            pass
+        for i in places['results']:
+            id = i['place_id']
+            parameters = ['name', 'formatted_address']
+            places_details = gmaps.place(place_id=id, fields=parameters)
+            result = places_details['result']
+            address = result['formatted_address']
+            names = result['name']
+            combined += ' ' + names + ' @ ' + address + ' --- '
+        results.append(combined)
